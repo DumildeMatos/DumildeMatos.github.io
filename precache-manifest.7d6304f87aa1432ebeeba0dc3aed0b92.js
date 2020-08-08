e2a7db18aa677d92ddd132990ebed43cbe5330ec
Dumilde Matos <dumilde.matos@jupiter.co.ao>
2020-08-08 17:08:42 +0100

    New look

---
+++ precache-manifest.7d6304f87aa1432ebeeba0dc3aed0b92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e51632ddff5973806a20a4596c1e638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9645714d656cb93b2fc7",
+    "url": "/static/css/main.54502cdc.chunk.css"
+  },
+  {
+    "revision": "73cafb779ca3d1f8a0b0",
+    "url": "/static/js/2.0cc2500b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0cc2500b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9645714d656cb93b2fc7",
+    "url": "/static/js/main.2c369da3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ba24d34e63beb027c3db48d4c60b0e2c",
+    "url": "/static/media/logo.ba24d34e.svg"
+  }
+]);
