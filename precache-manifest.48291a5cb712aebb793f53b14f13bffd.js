6180c3e07ae05c3612c61e9218d7ad82c8be8da2
Dumilde Matos <dumilde.matos@jupiter.co.ao>
2020-08-16 16:47:59 +0100

    Update about info

---
+++ precache-manifest.48291a5cb712aebb793f53b14f13bffd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d780726a76af5c64cd6998e5ef6efb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6a9cda873410843a32a",
+    "url": "/static/css/main.13ceb21e.chunk.css"
+  },
+  {
+    "revision": "f712f968a6b91abfd18d",
+    "url": "/static/js/2.56b3c045.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.56b3c045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a9cda873410843a32a",
+    "url": "/static/js/main.a207190f.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "cfce6abbbff0099b15691345d8b94dcc",
+    "url": "/static/media/GOTHIC.cfce6abb.TTF"
+  },
+  {
+    "revision": "f375889b78433b63a0842498c2f764cc",
+    "url": "/static/media/dumildematos.f375889b.jpeg"
+  },
+  {
+    "revision": "ba24d34e63beb027c3db48d4c60b0e2c",
+    "url": "/static/media/logo.ba24d34e.svg"
+  },
+  {
+    "revision": "8d518fbf62bfe6b2d895646ebd0422a0",
+    "url": "/static/media/mobile-wireframes.8d518fbf.jpg"
+  },
+  {
+    "revision": "d904571bed90d7dacc8d90248c66f9f6",
+    "url": "/static/media/ui_ux.d904571b.png"
+  },
+  {
+    "revision": "5518f3645254f04dd1f597b90f8d5223",
+    "url": "/static/media/web_development.5518f364.jpg"
+  }
+]);
